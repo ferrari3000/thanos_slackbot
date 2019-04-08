46bcf2ec5b553734f9b6ac2c95e5b90a0ac14372
ferrari3000 <ferrari3000.tf@gmail.com>
2019-04-08 15:42:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
+const server = require('./Server');
 
 const bot = new SlackBot({
-    token: 'xoxb-436996219559-575477629911-yz8T2FCbwKPBMA3kaajkBce8', 
+    token: '[Redacted]', 
     name: 'thanos_bot'
 });
 
@@ -15,16 +16,18 @@ var channelName;
 try {
 
 bot.on('message', (data) => {
-    if(data.type != 'message') {
+    if(data.type != 'message' || data.text == null || data.subtype == 'bot_message') {
         return;
     }
 
+    bot.on('error', (err) => console.log(err));
+
     var channelInfo;
-    channelInfo = axios.get('https://slack.com/api/channels.info?token=xoxb-436996219559-575477629911-SbXrnlENWM3y3vamvqYV6AgA&channel='+data.channel+'&pretty=0').then(res=>{
+    channelInfo = axios.get('https://slack.com/api/channels.info?token=[Redacted]&channel='+data.channel+'&pretty=0').then(res=>{
     channelId = res.data.channel.id;
     channelName = res.data.channel.name;
-    
     channel = data.channel
+    console.log(channel);
 
     if(channelId == channel) {
         channel = channelName;
@@ -35,39 +38,40 @@ bot.on('message', (data) => {
 
     message = data.text;
 
-    axios.post('https://login.salesforce.com/services/oauth2/token?[redacted]').then(res=> {
+    axios.post('[Redacted]').then(res=> {
     const bearer = res.data.access_token;
 
     console.log(data);
 // Get quote
 
-if(message != null && message.includes(' InfinityWar')) {
-
+if(message.includes(' InfinityWar') || message.includes(' Whatever it takes'))
+{
     var request = require("request");
 
         var options = { method: 'POST',
         url: 'https://tfoor-dev-ed.my.salesforce.com/services/apexrest/ThanosQuote/',
         headers: 
-        { 'Postman-Token': 'd4a91c9a-c579-40f6-be53-fab241a654bf',
+        { 'Postman-Token': '[Redacted]',
             'cache-control': 'no-cache',
             'Content-Type': 'application/json',
             Authorization: 'Bearer '+ bearer },
-        body: { quotes: [ { message: 'InfinityWar' } ] },
+        body: { quotes: [ { message: message } ] },
         json: true };
 
             request(options, function (error, response, body) {
             if (error) throw new Error(error);
-            
-            bot.postMessageToChannel(`${channel}`,`${body}`);
+                
+                
+                bot.postMessageToChannel(`${channel}`,`${body}`);
+
             });
 
-        }
-
+        } 
     });
   }) 
-});    
-
+});
 }
-catch {
-    bot.on('error', (err) => console.log(err));
+catch (e) {
+    console.log(channelInfo);
+    console.log();
 }
